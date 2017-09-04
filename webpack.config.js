271902818f9c832e2aba1078fa0ff36fdc7eec43
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-09-03 23:30:36 -0700

    adding css for module loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,25 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.(js|jsx|css)$/,
+        test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react', 'stage-3']
         }
+      }, 
+      {
+        test: /\.css$/,
+        loader:'style!css!'
       }
     ]
   },
-  resolveLoader: {
-    moduleExtensions: ['-loader']
-  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: ['.js', '.json', '.jsx'],
+  }
 };
