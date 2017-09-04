c50350900e0ac6516432c6b02e9e420efc4c9460
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-09-04 00:25:13 -0700

    adding modules:false to presets in webpack to combat uglify error

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react', 'stage-3']
+          presets: ['es2015', 'react', 'stage-3', {"modules":false}]
         }
       }, 
       {
